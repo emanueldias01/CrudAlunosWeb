ef6e139299a2bb0bee6c1f673082447e2812cfc2
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-06-24 14:21:44 -0300

    crud done

--- src/repository/repository.js
+++ src/repository/repository.js
@@ -15,28 +15,30 @@ class AlunoRepository{
     }
 
     static createAluno(aluno){
-        if(aluno.nome === null || aluno.ira === null || aluno.curso === null) throw new Error('Campos inválidos')
+        if(!aluno.nome || aluno.ira === undefined || !aluno.curso) throw new Error('Campos inválidos')
         if(this.alunosList.findIndex(a => a.nome === aluno.nome) !== -1) throw new Error('Já existe um aluno com esse nome')
-
-        const alunoCreate = new Aluno(aluno.nome, null,aluno.ira)
 
         const curso = Aluno.CURSO[aluno.curso.toLowerCase()]
         if(curso === undefined) throw new Error('Curso inválido')
 
-        alunoCreate.curso = Aluno.CURSO[aluno.curso.toLowerCase()]
+        const alunoCreate = new Aluno(aluno.nome, curso, aluno.ira)
         this.alunosList.push(alunoCreate)
     }
 
     static editar(aluno){
         const alunoFind = this.alunosList.find(a => a.nome === aluno.nome)
-        if(alunoFind === undefined) throw new Error('Aluno não encontrado')
-        if(aluno.curso !== "" && aluno.curso !== null) alunoFind.curso = Aluno.CURSO[aluno.curso]
-        if(aluno.ira !== 0 && aluno.ira !== null) alunoFind.ira = aluno.ira
+        if(!alunoFind) throw new Error('Aluno não encontrado')
+        if(aluno.curso && aluno.curso !== "") {
+            const curso = Aluno.CURSO[aluno.curso.toLowerCase()]
+            if(curso === undefined) throw new Error('Curso inválido')
+            alunoFind.curso = curso
+        }
+        if(aluno.ira !== undefined && aluno.ira !== null) alunoFind.ira = aluno.ira
     }
 
     static apagar(nome){
         const index = this.alunosList.findIndex(a => a.nome === nome)
-        if(index !== -1) throw new Error('Aluno não encontrado')
+        if(index === -1) throw new Error('Aluno não encontrado')
         this.alunosList.splice(index, 1)
     }
 
