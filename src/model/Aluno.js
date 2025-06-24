4e99aae7f7bd054387e718fc8476bb0a6f9a628f
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-06-24 13:46:35 -0300

    make methods crud

--- src/model/Aluno.js
+++ src/model/Aluno.js
@@ -1,4 +1,14 @@
 class Aluno{
+
+    static CURSO = {
+        si : "Sistemas de Informação",
+        cc : "Ciência da Computação",
+        ec : "Engenharia de Computação",
+        es : "Engenharia de Software",
+        rc : "Redes de Computadores",
+        dd : "Desing Digital"
+    }
+    
     constructor(nome, curso, ira){
         this.nome = nome
         this.curso = curso
--- src/repository/repository.js
+++ src/repository/repository.js
@@ -2,14 +2,42 @@ import Aluno from "../model/Aluno.js";
 
 class AlunoRepository{
 
+    
+
     static alunosList = [
-            new Aluno('Emanuel','Sistemas de Informação', 9),
-            new Aluno('Maria', 'Ciência da Computação', 8),
-            new Aluno('João', 'Engenharia de Computação', 7)
+            new Aluno('Emanuel', Aluno.CURSO.si, 9),
+            new Aluno('Maria', Aluno.CURSO.cc, 8),
+            new Aluno('João', Aluno.CURSO.ec, 7)
         ]
 
     static getAlunos(){
         return AlunoRepository.alunosList
+    }
+
+    static createAluno(aluno){
+        if(aluno.nome === null || aluno.ira === null || aluno.curso === null) throw new Error('Campos inválidos')
+        if(this.alunosList.findIndex(a => a.nome === aluno.nome) !== -1) throw new Error('Já existe um aluno com esse nome')
+
+        const alunoCreate = new Aluno(aluno.nome, null,aluno.ira)
+
+        const curso = Aluno.CURSO[aluno.curso.toLowerCase()]
+        if(curso === undefined) throw new Error('Curso inválido')
+
+        alunoCreate.curso = Aluno.CURSO[aluno.curso.toLowerCase()]
+        this.alunosList.push(alunoCreate)
+    }
+
+    static editar(aluno){
+        const alunoFind = this.alunosList.find(a => a.nome === aluno.nome)
+        if(alunoFind === undefined) throw new Error('Aluno não encontrado')
+        if(aluno.curso !== "" && aluno.curso !== null) alunoFind.curso = Aluno.CURSO[aluno.curso]
+        if(aluno.ira !== 0 && aluno.ira !== null) alunoFind.ira = aluno.ira
+    }
+
+    static apagar(aluno){
+        const index = this.alunosList.findIndex(a => a.nome === aluno.nome)
+        if(index !== -1) throw new Error('Aluno não encontrado')
+        this.alunosList.splice(index, 1)
     }
 
 }
